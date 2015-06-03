db32054d1cc71c9ab820e01c9cf2f705cc04af20
tadeubarbosa <tadeufbarbosa@gmail.com>
2015-06-03 19:24:56 -0300

    EDIT HELPERS/HTML

--- src/Helpers/Html.php
+++ src/Helpers/Html.php
@@ -28,7 +28,7 @@
 
 			$attrs = '';
 
-			if( isset(self::$data['attrs']) && count(self::$data['attrs']) ):
+			if( isset( self::$data['attrs'] ) && count( self::$data['attrs'] ) ):
 
 				$attrs = ' ';
 
